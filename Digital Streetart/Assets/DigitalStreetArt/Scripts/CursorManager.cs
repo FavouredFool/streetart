e17c63f5aff519e081199615dc4c878a0c851c99
Tim Wahrburg <t.wahrburg@gmail.com>
2023-11-30 15:36:05 +0100

    feat: New way of calculating red cursor

--- Digital Streetart/Assets/DigitalStreetArt/Scripts/CursorManager.cs
+++ Digital Streetart/Assets/DigitalStreetArt/Scripts/CursorManager.cs
@@ -8,7 +8,7 @@ using UnityEngine.InputSystem;
 
 public class CursorManager : MonoBehaviour
 {
-    [SerializeField] Transform cursor;
+    [SerializeField] Transform cursorTranslatedController;
     [SerializeField] Transform raycastCursor;
     [SerializeField] TrackedPoseDriver stabilizedController;
     [SerializeField] Vector3 planePosition;
@@ -24,10 +24,11 @@ public class CursorManager : MonoBehaviour
     // Das wäre dann ein Recalibration-Process.
     // Da die technisch gesehen sich nur ändern wenn die Lighthouses sich bewegen,
     // könnte man die auch in einer Datei speichern damit man die nur einmal pro Session neu berechnen muss.
-    Vector3 topRightRealReferencePoint = new Vector2(0.3f, 2.0f);
-    Vector3 bottomLeftRealReferencePoint = new Vector2(-0.6f, 1.4f);
-    Vector3 bottomRightRealReferencePoint = new Vector2(0, 0);
+    Vector3 topRightRealReferencePoint = new Vector2(0, 0);
+    Vector3 bottomLeftRealReferencePoint = new Vector2(0, 0);
     Vector3 localForwardVector = Vector3.forward;
+
+    Quaternion rotationOffset = Quaternion.identity;
 
     void Start()
     {
@@ -36,46 +37,45 @@ public class CursorManager : MonoBehaviour
 
     void Update()
     {
-        Vector2 positionOnWall = ProjectCursorPositionOnWall(stabilizedController.transform.position);
+        cursorTranslatedController.position = DistortPositionBasedOnReferencePoints(stabilizedController.transform.position);
+        cursorTranslatedController.rotation = stabilizedController.transform.rotation * rotationOffset;
 
-        cursor.position = DistortPositionBasedOnReferencePoints(positionOnWall);
-
-        if(RaycastPositionOnWall(out var pos))
+        if (RaycastPositionOnWall(out var pos))
         {
-            raycastCursor.position = DistortPositionBasedOnReferencePoints(pos);
+            raycastCursor.position = pos;
         }
+
+        if (!Physics.Raycast(new Ray(cursorTranslatedController.position, cursorTranslatedController.forward), out RaycastHit hit)) return;
+
+        Debug.Log("hit: " + hit);
     }
 
-    Vector2 DistortPositionBasedOnReferencePoints(Vector2 positionOnWall)
-    {
-        float t_x = Mathf.InverseLerp(bottomLeftRealReferencePoint.x, topRightRealReferencePoint.x, positionOnWall.x);
-        float t_y = Mathf.InverseLerp(bottomLeftRealReferencePoint.y, topRightRealReferencePoint.y, positionOnWall.y);
-
-        Vector2 virtualPosition;
-        virtualPosition.x = Mathf.Lerp(bottomLeftVirtualReferenceTransform.position.x, topRightVirtualReferenceTransform.position.x, t_x);
-        virtualPosition.y = Mathf.Lerp(bottomLeftVirtualReferenceTransform.position.y, topRightVirtualReferenceTransform.position.y, t_y);
-
-        return virtualPosition;
-    }
-
-    bool RaycastPositionOnWall(out Vector2 position)
+    bool RaycastPositionOnWall(out Vector3 position)
     {
         position = Vector2.zero;
         float distance;
-        Vector3 controllerPosition = stabilizedController.transform.position;
-        Vector3 forwardDirection = stabilizedController.transform.TransformDirection(localForwardVector);
+        Vector3 controllerPosition = cursorTranslatedController.position;
+        Vector3 forwardDirection = cursorTranslatedController.forward;
         if (plane.Raycast(new Ray(controllerPosition, forwardDirection), out distance))
         {
-            Vector3 hitLocation = controllerPosition + distance * forwardDirection;
-            position = new Vector2(hitLocation.x, hitLocation.y);
+            position = controllerPosition + distance * forwardDirection;
             return true;
         }
         return false;
     }
 
-    Vector3 ProjectCursorPositionOnWall(Vector3 controllerPosition)
+    Vector3 DistortPositionBasedOnReferencePoints(Vector3 controllerPosition)
     {
-        return new Vector3(controllerPosition.x, controllerPosition.y, 0);
+        float t_x = Mathf.InverseLerp(bottomLeftRealReferencePoint.x, topRightRealReferencePoint.x, controllerPosition.x);
+        float t_y = Mathf.InverseLerp(bottomLeftRealReferencePoint.y, topRightRealReferencePoint.y, controllerPosition.y);
+        float t_z = Mathf.InverseLerp(bottomLeftRealReferencePoint.z, topRightRealReferencePoint.z, controllerPosition.z);
+
+        Vector3 virtualPosition;
+        virtualPosition.x = Mathf.Lerp(bottomLeftVirtualReferenceTransform.position.x, topRightVirtualReferenceTransform.position.x, t_x);
+        virtualPosition.y = Mathf.Lerp(bottomLeftVirtualReferenceTransform.position.y, topRightVirtualReferenceTransform.position.y, t_y);
+        virtualPosition.z = Mathf.Lerp(bottomLeftVirtualReferenceTransform.position.z, topRightVirtualReferenceTransform.position.z, t_z);
+
+        return virtualPosition;
     }
 
     public void OnCalibrationButton(InputAction.CallbackContext context)
@@ -94,20 +94,38 @@ public class CursorManager : MonoBehaviour
                 break;
             case 2:
                 topRightRealReferencePoint = stabilizedController.transform.position;
-                tutorialText.text = "Press menu to calibrate lower right corner";
-                calibrationState++;
-                break;
-            case 3:
-                bottomRightRealReferencePoint = stabilizedController.transform.position;
                 tutorialText.text = "Hold your Controller upright and press menu to finish calibration";
                 calibrationState++;
                 break;
-            case 4:
-                plane = new Plane(bottomLeftRealReferencePoint, topRightRealReferencePoint, bottomRightRealReferencePoint);
-                localForwardVector = stabilizedController.transform.InverseTransformDirection(-plane.normal);
+            case 3:
+                rotationOffset = Quaternion.Inverse(Quaternion.LookRotation(stabilizedController.transform.forward));
                 tutorialText.text = "";
                 calibrationState = 0;
                 break;
         }
+    }
+
+    public void OnTriggerButton(InputAction.CallbackContext context)
+    {
+        
+        if (!context.action.WasPerformedThisFrame()) return;
+
+        if (!Physics.Raycast(new Ray(cursorTranslatedController.position, cursorTranslatedController.forward), out RaycastHit hit)) return;
+
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null) return;
+
+        Texture2D tex = rend.material.mainTexture as Texture2D;
+        Vector2 pixelUV = hit.textureCoord;
+        pixelUV.x *= tex.width;
+        pixelUV.y *= tex.height;
+
+        tex.SetPixel((int)pixelUV.x, (int)pixelUV.y, Color.red);
+        tex.Apply();
+
+        Debug.Log(pixelUV);
+        
     }
 }
