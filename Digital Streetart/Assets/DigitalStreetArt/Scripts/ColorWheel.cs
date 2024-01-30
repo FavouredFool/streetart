8842f781c9badc5d1539d7b72003285bee09d69a
Tim Wahrburg <t.wahrburg@gmail.com>
2024-01-30 14:51:45 +0100

    feat: Fixed Build Error

--- Digital Streetart/Assets/DigitalStreetArt/Scripts/ColorWheel.cs
+++ Digital Streetart/Assets/DigitalStreetArt/Scripts/ColorWheel.cs
@@ -43,7 +43,7 @@ public class ColorWheel : MonoBehaviour
         DrawCircle(_texture, CurrentColor, _resolution/2, _resolution/2, radius: _resolution/10);
         foreach(Color c in colors)
         {
-            DrawCircle(_texture, c.WithAlpha(_hoverColor == c ? 1f : 0.7f), _resolution/2, _resolution/2, idx * width + 1, width - 2, _resolution/2 - (_hoverColor == c ? 0 : _resolution/20), _resolution/3);
+            DrawCircle(_texture, 1, _resolution/2, _resolution/2, idx * width + 1, width - 2, _resolution/2 - (_hoverColor == c ? 0 : _resolution/20), _resolution/3);
             idx++;
         }
         _texture.Apply();
